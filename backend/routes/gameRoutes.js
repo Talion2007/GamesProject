bbd3c8446c979f4977565d717094662e20839ed3
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2024-12-12 14:19:38 -0300

    o que voce quer ta duro

--- backend/routes/gameRoutes.js
+++ backend/routes/gameRoutes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const gameController = require ("../controllers/gameControllers")
 
-router.get('/historias/palavrachave/:palavrachave', gameController.getHistory); 
+router.get('/jogos', gameController.getAllJogos); 
 
-router.get('/mensagem', gameController.getMessage); 
+router.get('/jogos/generos/:genero', gameController.getJogosByGenero); 
 
-router.post('/mensagem', gameController.createMessage); 
+router.get('/jogos/plataformas/:plataforma', gameController.getJogosByPlataforma); 
+
+router.post('/jogos', gameController.addJogo);
+
+router.put('/jogos/:id', gameController.updateJogo);
+
+router.delete('/jogo/:id', gameController.deleteJogo);
 
 module.exports = router;
